788bf2e4be7d3063700675be3d5880641cd11a49
Abdelrahmanawad127 <Aa2400296@tkh.edu.eg>
2025-12-03 04:46:13 +0200

    Update code part 1.py

--- code part 1.py
+++ code part 1.py
@@ -25,5 +25,31 @@ if message.lower().endswith(".txt"):
     except:
         print("can't open file for unknown reason")
         exit()
+full_message = message + "this_is_the_end"
+message_in_binary = ""
+for character in full_message:
+    message_in_binary += format(ord(character), "08b")
+try:
+    with open(default_image, "rb") as f:
+        data = f.read()
+except FileNotFoundError:
+    print("please input an existing file")
+    exit()
+except PermissionError:
+    print("this file cannot be opened")
+    exit()
+except:
+    print("An unknown error has occurred when opening your image")
+    exit()
+if len(data) == 0:
+    print("file is empty")
+    exit()
+pixel_offset = data[10] + (data[11] << 8) + (data[12] << 16) + (data[13] << 24)
+pixels = []
+for i in range(pixel_offset, len(data) - 2, 3):
+    blue = data[i]
+    green = data[i + 1]
+    red = data[i + 2]
+    pixels.append((red, green, blue))
 
 
